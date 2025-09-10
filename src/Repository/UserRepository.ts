1ca0bc5f2b0fbec9af8a7a3a1a0408104f5f8bcb
Crys-Faccat <crystopher.kochler@sou.faccat.br>
2025-09-10 18:30:55 -0300

    Update UserRepository.ts

--- src/Repository/UserRepository.ts
+++ src/Repository/UserRepository.ts
@@ -1,8 +1,40 @@
 import { User } from "../Model/User";
 
-class UserRepository{
+class UserRepository {
+    private db = require('../Database/ecommerceDB.db');
 
+    async getUsers(): Promise<User[]> {
+        const result = await this.db.all("SELECT * FROM user");
+        return result;
+    }
 
+    async getUserById(id: string): Promise<User> {
+        const foundUser: User | undefined = await this.db.get(
+            `SELECT * FROM user WHERE id=?`,
+            [id]
+        );
+
+        if (!foundUser) {
+            throw new Error(`Usuário ${id} não encontrado!`);
+        }
+
+        return foundUser;
+    }
+
+    /*
+    async addUser(user: User): Promise<string> {
+        await this.db.run(
+            `INSERT INTO user (id, userName, password, permissionList, createdAt, updatedAt) VALUES (?, ?, ?, ?, ?, ?)`,
+            [user.id, user.userName, user.password, JSON.stringify(user.permissionList), user.createdAt, user.updatedAt]
+        );
+        return `http://localhost:3000/user/${user.id}`;
+    }
+
+    async deleteUser(id: string): Promise<string> {
+        await this.db.run(`DELETE FROM user WHERE id=?`, [id]);
+        return `Usuário ${id} deletado com sucesso`;
+    }
+    */
 }
 
 export default UserRepository;
