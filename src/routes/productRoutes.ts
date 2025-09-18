e04c86cfa2e607473d4ada451c30464b28779d5d
Joao H. Kohlrausch <joaohenriquekohlrausch@gmail.com>
2025-09-18 08:03:59 -0300

    Merge commit

--- src/routes/productRoutes.ts
+++ src/routes/productRoutes.ts
@@ -1,15 +1,14 @@
 import { Router } from "express";
 import ProductController from "../Controller/ProductController";
-import { authenticationMiddleware } from "../Middleware/AuthenticationMiddleware";
 
 const productRouter = Router();
 const productController = new ProductController();
 
-productRouter.get('/produto', authenticationMiddleware, productController.getProducts);
+productRouter.get('/produto', productController.getProducts);
 productRouter.get('/produto/:id', productController.getProductById);
-productRouter.post('/produto', productController.addProduct);
+productRouter.post('produto', productController.addProduct);
 productRouter.put('/produto/:id', productController.updateProduct);
 productRouter.patch('/produto/:id', productController.patchProductStatus)
-productRouter.delete('/produto/:id', productController.deleteProduct);
+productRouter.delete('product/:id', productController.deleteProduct);
 
 export default productRouter;
