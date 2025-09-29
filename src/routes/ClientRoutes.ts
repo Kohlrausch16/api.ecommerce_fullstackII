00bd0d96277e8cbdfe8b312b4040bb9505dac0b0
Kohlrausch <joaohenriquekohlrausch@gmail.com>
2025-09-29 00:25:31 -0300

    Client permissions - fix

--- src/routes/ClientRoutes.ts
+++ src/routes/ClientRoutes.ts
@@ -8,7 +8,7 @@ const clientController = new ClientController
 
 clientRouter.get('/cliente', authenticationMiddleware, authorizationMiddleware(' get-clients'), clientController.getClients);
 clientRouter.get('/cliente/:id', authenticationMiddleware, authorizationMiddleware(' get-client-by-id'), clientController.getClientById);
-clientRouter.post('/cliente', authenticationMiddleware, authorizationMiddleware(' add-client'), clientController.addClients);
+clientRouter.post('/cliente', clientController.addClients);
 clientRouter.put('/cliente/:id', authenticationMiddleware, authorizationMiddleware(' put-client'), clientController.updateClient);
 clientRouter.delete('/cliente/:id', authenticationMiddleware, authorizationMiddleware(' delete-client'), clientController.deleteClient);
 
